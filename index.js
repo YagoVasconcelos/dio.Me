cd3b498f1c064d3fa39a9cdead3545609bd604fe
Yago_Marinho <yagopsm@gmail.com>
2026-04-20 21:13:43 -0300

    Desafio 1  dio.me

--- index.js
+++ index.js
@@ -1,1 +1,39 @@
-console.log ("Oi");
+// 1. Variáveis, nome e a quantidade de experiência (XP)
+let nomeHeroi = "Yago_ADS";
+let xp = 7500;
+let nivel = "";
+
+// 2. Estrutura de decisão para classificar o nível
+if (xp < 1000) {
+    nivel = "Ferro";
+} 
+
+else if (xp >= 1001 && xp <= 2000) {
+    nivel = "Bronze";
+} 
+
+else if (xp >= 2001 && xp <= 5000) {
+    nivel = "Prata Ouro";
+} 
+
+else if (xp >= 6001 && xp <= 7000) {
+    nivel = "Platina Diamante";
+} 
+
+else if (xp >= 7001 && xp <= 8000) {
+    nivel = "Ascendente";
+} 
+
+else if (xp >= 8001 && xp <= 9000) {
+    nivel = "Imortal";
+} 
+
+else if (xp >= 9001 && xp <= 10000) {
+    nivel = "Imortal";
+} 
+
+else {
+    nivel = "Radiante";
+}
+
+console.log(`O Herói de nome **${nomeHeroi}** está no nível de **${nivel}**`);
